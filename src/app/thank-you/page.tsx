9eb6f8a8928871d50ca047d54fd5e821284fcfb1
Hiroki Chiyoda <contact@arafipro.com>
2025-10-22 17:11:22 +0900

    サンクスページとメッセージコンポーネントを追加
    - ユーザー登録完了後に表示されるサンクスページを新規作成
    - サンクスメッセージコンポーネントを追加し、登録完了のメッセージを表示

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,9 @@
+import ThankYouMessage from "@/components/feature/ThankYouMessage";
+
+export default function ThankYouPage() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <ThankYouMessage />
+    </main>
+  );
+}
---
+++ src/components/feature/ThankYouMessage.tsx
@@ -0,0 +1,8 @@
+export default function ThankYouMessage() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">ご登録ありがとうございます！</h1>
+      <p className="mt-4 text-lg">待機リストへの登録が完了しました。</p>
+    </div>
+  );
+}
