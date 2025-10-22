07d18f1ef368dfbca5b3a9cb6e1a3fb2e1df64b9
Hiroki Chiyoda <contact@arafipro.com>
2025-10-22 19:00:37 +0900

    レイアウトのフォントを更新
    - フォントをGeistからNoto Sans JPに変更
    - HTMLの言語属性を「ja」に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
-import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ja" suppressHydrationWarning>
+      <body className={`${notoSansJP.className} antialiased`}>
         {children}
         <Toaster />
       </body>
