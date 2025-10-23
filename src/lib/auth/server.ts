6671c0c8c0cca58caa920a3a594eacfe68f494ad
Hiroki Chiyoda <contact@arafipro.com>
2025-10-24 08:55:31 +0900

    非同期版の認証設定ロジックに変更
    - `betterAuth`の設定を非同期関数に変更し、`getDb`を使用してデータベースインスタンスを取得
    - コードの可読性と非同期処理の整合性を向上

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -1,19 +1,22 @@
-import { getDbSync } from "@/drizzle/db";
+import { getDb } from "@/drizzle/db";
 import * as authSchema from "@/drizzle/schema/authSchema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 // https://www.better-auth.com/docs/adapters/drizzle#example-usage を参考
-export const auth = betterAuth({
-  database: drizzleAdapter(getDbSync(), {
-    provider: "sqlite",
-    usePlural: true,
-	}),
-	emailAndPassword: { 
-    enabled: true, 
-  }, 
-  schema: {
-    ...authSchema,
-    user: authSchema.users,
-  },
-});
+export const auth = async () => {
+  const db = await getDb();
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "sqlite",
+      usePlural: true,
+    }),
+    emailAndPassword: {
+      enabled: true,
+    },
+    schema: {
+      ...authSchema,
+      user: authSchema.users,
+    },
+  });
+};
